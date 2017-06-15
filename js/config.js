a0d3b812fed2092f1d9c820b736c2e64b9638d26
adrivanrex <adrivanrex@gmail.com>
2017-06-15 17:57:42 +0800

    added realtime httpget

--- js/config.js
+++ js/config.js
@@ -178,6 +178,30 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
                 }
             }
         })
+        .state('dashboards.govtrackerEdit', {
+            url: "/govtrackerEdit",
+            templateUrl: "views/govtrackerEdit.html",
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+
+                            serie: true,
+                            name: 'angular-flot',
+                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
+                        },
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        }
+                    ]);
+                }
+            }
+        })
         .state('dashboards.dashboard_2', {
             url: "/dashboard_2",
             templateUrl: "views/dashboard_2.html",
